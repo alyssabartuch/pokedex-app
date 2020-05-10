9a8ce5d7d1b566473d4bca8cd46bed85289e750e
alyssabartuch <alyssabartuch@gmail.com>
2020-05-09 23:34:03 -0500

    Updates

---
+++ precache-manifest.4c676ab46765b8ab56aa1ed975d88e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f918a81cbab9b2aa067944e60e9d47",
+    "url": "/pokedex-app/index.html"
+  },
+  {
+    "revision": "6f8fad6aa363c8986cf5",
+    "url": "/pokedex-app/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "98fe568e9169ce3d819a",
+    "url": "/pokedex-app/static/css/main.bc7eba7b.chunk.css"
+  },
+  {
+    "revision": "6f8fad6aa363c8986cf5",
+    "url": "/pokedex-app/static/js/2.06038d8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex-app/static/js/2.06038d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98fe568e9169ce3d819a",
+    "url": "/pokedex-app/static/js/main.669d5f25.chunk.js"
+  },
+  {
+    "revision": "6f3546df5c56f0777923",
+    "url": "/pokedex-app/static/js/runtime-main.3d879a13.js"
+  }
+]);
