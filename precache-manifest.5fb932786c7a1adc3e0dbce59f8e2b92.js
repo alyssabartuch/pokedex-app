98b69624f5ae354a4ded4a346977b114f4848cc6
alyssabartuch <alyssabartuch@gmail.com>
2020-05-12 21:46:02 -0500

    Updates

---
+++ precache-manifest.5fb932786c7a1adc3e0dbce59f8e2b92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3308e81f9cbb13f8055314817b3fa577",
+    "url": "/pokedex-app/index.html"
+  },
+  {
+    "revision": "2991bfbbb2ece0a38951",
+    "url": "/pokedex-app/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c94438877233580e84f0",
+    "url": "/pokedex-app/static/css/main.54659eec.chunk.css"
+  },
+  {
+    "revision": "2991bfbbb2ece0a38951",
+    "url": "/pokedex-app/static/js/2.daa8fea3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex-app/static/js/2.daa8fea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94438877233580e84f0",
+    "url": "/pokedex-app/static/js/main.037dec8b.chunk.js"
+  },
+  {
+    "revision": "6f3546df5c56f0777923",
+    "url": "/pokedex-app/static/js/runtime-main.3d879a13.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokedex-app/precache-manifest.defeabf9e4a6639e524e3b41656ab2b4.js"
+  "/pokedex-app/precache-manifest.5fb932786c7a1adc3e0dbce59f8e2b92.js"
 );
 
 self.addEventListener('message', (event) => {
